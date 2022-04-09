2eff2f6ccd8faefe8321121f4d8ae45ef910675d
moudy-wold <83995707+moudy-wold@users.noreply.github.com>
2022-04-09 12:00:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,32 @@ function showData() {
     
     // get data
     fetch('https://randomuser.me/api/')
+          // convert data to JSON format
         .then(res => res.json())
+
         .then(data => {
-          
-            let dat = data.results[0];         
-            output.innerHTML = `
+            let dat = data.results;
+        //   sort data According to age 
+            function compare(a, b) {
+                if (a.dob.age < b.dob.age) {
+                    return -1;
+                }
+                if (a.dob.age > b.dob.age) {
+                    return 1;
+                }
+                return 0;
+            }
+            let arr = dat.sort(compare);
+            arr.forEach(item => {
+                output.innerHTML += `
             <ul>
-                <li>Name: ${dat.name.title} ${dat.name.first} ${dat.name.last}</li>
-                <li><img src='${dat.picture.large}'></li>
-                <li>Phone Number: <span> ${dat.cell}</span></li>
-                <li>Age: <span> ${dat.dob.age}</span></li>
-                <li>Email: <span> ${dat.email}</span></li>
-                <li>Gender: <span> ${dat.gender}</span></li>
-                <li>Country: <span> ${dat.location.city}</span></li>
+                <li>Name: ${item.name.title} ${item.name.first} ${item.name.last}</li>
+                <li><img src='${item.picture.large}'></li>
+                <li>Phone Number: <span> ${item.cell}</span></li>
+                <li>Age: <span> ${item.dob.age}</span></li>
+                <li>Email: <span> ${item.email}</span></li>
+                <li>Gender: <span> ${item.gender}</span></li>
+                <li>Country: <span> ${item.location.city}</span></li>
             </ul>
             `
         })
